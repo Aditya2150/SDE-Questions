dcc77b61d416f6eee5f6279f04136e54245b9127
Aditya Jain <adityaavi2100@gmail.com>
2021-06-24 16:38:00 +0530

    addition of recursive method

--- Day 3/power_function_modulo.cpp
+++ Day 3/power_function_modulo.cpp
@@ -7,16 +7,34 @@ using namespace std;
 
 class Solution{   
 public:
-    long long int power(int a, long long int b) { 
-        long long int res=1,x=a;
+// Recursive Solution Time- O(logn)
+    double solve(double x,long long n)
+    {
+        if(n==0)
+            return 1;
+        if(n%2)
+            return x*solve(x,n-1);
+        else
+            return solve(x*x,n/2);
+    }
+    double myPow(double x, int n) {
+        long long nn=abs(n);
+        double ans=solve(x,nn);
+        return (n<0?1.0/ans:ans);
+    }
+
+// Iterative using bit_manipulation Time-O(logn)
+    double power(double a,int n) { 
+        long long b=abs(n);
+        double res=1;
         while(b)
         {
             if(b&1)
-            res=(res*x)%mod;
+            res=res*a; //res=(res*a)%mod;
             b>>=1;
-            x=(x*x)%mod;
+            a=a*a; //a=(a*a)%mod;
         }
-        return res;
+        return (n<0?1.0/res:res);
     }
 };
 
@@ -27,11 +45,12 @@ int main()
     cin>>t;
     while(t--)
     {
-        int a;
+        double a;
         long long b;
         cin >> a >> b;
         Solution ob;
         cout << ob.power(a, b) << endl;
+        cout << ob.myPow(a, b) << endl;
     
     }
     return 0; 
